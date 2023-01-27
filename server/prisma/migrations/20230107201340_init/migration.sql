dc27568f400c0bc2b01159033d553f2c948db5d8
Victor Beirão Fernandes <victorbeiraof@gmail.com>
2023-01-27 13:37:07 -0300

    chore: initial config

---
+++ server/prisma/migrations/20230107201340_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "cellphone" TEXT NOT NULL,
+    "photo" TEXT NOT NULL
+);
