2bb3cdb1bde85c45fb53b593552d3c27eaa1aa04
Victor Beirão Fernandes <victorbeiraof@gmail.com>
2023-01-27 13:23:11 -0300

    chore: setup tests

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+class FormDataMock {
+  append: (name: string, value: string | Blob, fileName?: string) => void = vitest.fn();
+  delete: (name: string) => void = vitest.fn();
+  get: (name: string) => FormDataEntryValue | null = vitest.fn();
+  getAll: (name: string) => FormDataEntryValue[] = vitest.fn();
+  has: (name: string) => boolean = vitest.fn();
+  set: (name: string, value: string | Blob, fileName?: string) => void = vitest.fn();
+  forEach: (callbackfn: (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg?: any) => void =
+    vitest.fn();
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+global.FormData = FormDataMock;
